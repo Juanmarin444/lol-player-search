25dac6ca65e81525db72fd68615a19dbe615f2a3
juanmarin444 <30785213+Juanmarin444@users.noreply.github.com>
2022-08-08 21:25:58 -0700

    Display response data

--- src/findPlayer/index.js
+++ src/findPlayer/index.js
@@ -1,32 +1,36 @@
 import { useState } from 'react';
-import PlayerInfo from '.././playerInfo';
 
 const apiKey = process.env.REACT_APP_API_KEY;
 
 const FindPlayer = () => {
 
   const [ username, setUsername ] = useState('');
-  const [ data, setData ] = useState(null);
+  const [ player, setPlayer ] = useState(null);
+  const [ playerFound, setPlayerFound ] = useState(false);
   
+  // fetch('http://54.173.30.106/health-check')
   
-  const searchPlayer = () => {
-    fetch(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${username}?api_key=${apiKey}`)
 
-    // fetch('http://54.173.30.106/health-check')
-    .then((response) => {
-        console.log(response)
-        if (!response.ok) {
-            return {};
-        } else {
-            return response.json();
-        }
-    })
-    .then((data) => {
-        console.log(data)
-        setData({
-            playerData: data
-        })
-    })
+  const searchPlayer = async () => {
+    try {
+      const response = await fetch(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${username}?api_key=${apiKey}`, {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error! status: ${response.status}`);
+      }
+
+      const result = await response.json();
+      setPlayer({playerData: result});
+      setPlayerFound(true);
+      return result;
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   const handleChange = event => {
@@ -36,10 +40,18 @@ const FindPlayer = () => {
   return (
     <>
       <h1>Account Share Buster</h1>
-      <button onClick={searchPlayer}>Click Here</button>
-      <p>Search for a player</p>
       <input onChange={handleChange}></input>
-      <PlayerInfo props={data}/>
+      <button onClick={searchPlayer}>Click Here</button>
+      {playerFound ?
+      <div>
+        <h1>Player has been found</h1>
+        <h3>{player.playerData.name}</h3>
+        <p>{player.playerData.puuid}</p>
+        <p>Probability of account sharing: 50%</p>
+      </div> :
+      <div>
+        Search for a player
+      </div>}
     </>
   );
   
